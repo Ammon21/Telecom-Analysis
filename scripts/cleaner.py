abd2e750502900676c133baf07441e900789551d
Ammon21 <ammonb6@gmail.com>
2022-08-01 16:00:59 +0300

    modular cleaning scripts

---
+++ scripts/cleaner.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+def percent_missing(df):
+
+    # Calculate total number of cells in dataframe
+    totalCells = np.product(df.shape)
+
+    # Count number of missing values per column
+    missingCount = df.isnull().sum()
+
+    # Calculate total number of missing values
+    totalMissing = missingCount.sum()
+
+    # Calculate percentage of missing values
+    print("The Telecom Financial dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
+
+def fix_missing_ffill(df, col):
+    df[col] = df[col].fillna(method='ffill')
+    return df[col]
+
+
+def fix_missing_bfill(df, col):
+    df[col] = df[col].fillna(method='bfill')
+    return df[col]
